bba2c29901649d2df3566e68517c1a849e999e7d
John Kelley <john@kelley.ca>
2017-01-22 07:59:35 +0000

    Refactor code for strange control protocol. Support --ip and --listen and print device name for discovered devices

--- lkctl.py
+++ lkctl.py
@@ -1,16 +1,15 @@
 #!/bin/env python2
 
-from Queue import Queue
+from Queue import Queue, Empty
 from socket import *
 from threading import Thread
+import argparse
 import select
 import signal
 import struct
 import sys
 
 broadcastPort = 6000
-#broadcastMulticastGroup = '224.1.1.1'
-#broadcastMulticastGroup = '239.255.255.255'
 broadcastMulticastGroup = '239.255.42.42'
 
 stop_threads = False
@@ -27,14 +26,19 @@ def broadcastListener(out_q, done):
     mreq = struct.pack("4sl", inet_aton(broadcastMulticastGroup), INADDR_ANY)
     s.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
 
+    ips = []
     s.setblocking(0)
     print "Listening for devices..."
     while not done():
         r_rdy, w_rdy, x_rdy  = select.select([s],[],[], 0.1)
         if r_rdy:
-            msg = r_rdy[0].recv(128) 
-            printHex(msg)
-            out_q.put(msg)
+            msg = r_rdy[0].recv(128)
+            ip1, ip2, ip3, ip4 = struct.unpack("BBBB", msg[2:6])
+            ip = "%d.%d.%d.%d" % (ip1, ip2, ip3, ip4)
+            if not ip in ips:
+                print "Found device at %s" % ip
+                ips.append(ip)
+                out_q.put(ip)
     s.close()
 
 def signal_handler(signal, frame):
@@ -46,49 +50,125 @@ def signal_handler(signal, frame):
 class IPTV_DeviceControl:
     _knockPort = 9002
     _controlPort = 9001
-    _connected = False
     def __init__(self, ip):
         self._ip = ip
     
-    def connect(self):
+    def __connect(self):
+        sock = socket(AF_INET, SOCK_STREAM)
+        ip = '0'
         try:
-            self._s = socket(AF_INET, SOCK_STREAM)
-            print "Connecting to %s, %d" % (self._ip, self._controlPort)
-            res = self._s.connect_ex((self._ip, self._controlPort))
+            res = sock.connect_ex((self._ip, self._controlPort))
             if res == 0:
-                self._connected = True;
-                self._my_ip = self._s.getsockname()[0]
-        except:
+                ip = sock.getsockname()[0]
+        except Exception, e:
+            print "Exception: %s" % e
+            sock.close()
+            return False, None, None
+        return res == 0, sock, ip
+
+    def __newTCPServer(self):
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sock.bind(('', 0))
+        return sock
+
+    def __performCommand(self, msg):
+        # connect to the remote device
+        valid, remote_sock, my_ip = self.__connect()
+
+        if not valid:
+            print "Could not establish connection to the device"
             return False
-        return res == 0
 
-    def knock(self):
-        pass    
+        # start TCP server to catch response
+        listen = self.__newTCPServer()
+        listenPort = listen.getsockname()[1]
+        listen.listen(1)
+
+        # construct header
+        payload = 'IPTV_CMD'
+        payload += struct.pack(">4sH", inet_aton(my_ip), listenPort)
+
+        # append message
+        payload += msg
+
+        # send payload
+        #print "Sending"
+        #printHex(payload)
+        remote_sock.send(payload)
+
+        # listen for response
+        conn, addr = listen.accept()
+        data = conn.recv(256)
+
+        #TODO check checksum
+
+        conn.close()
+        listen.close()
+        remote_sock.close()
+
+        return data
+
     def GetName(self):
-        if not self._connected:
-            raise Exception('Not connected!')
+        # construct message
+        msg = struct.pack(">HHHBB", 0x7400, 0x1f00, 0x0221, 0, 0) # last 0 is a checksum
 
+        # perform command
+        data = self.__performCommand(msg)
+        if data:
+            if len(data) != 53:
+                print "Invalid length response of %d (should be 53)" % len(data)
+                printHex(data)
+                return None
 
-        msg = 'IPTV_CMD'
-        msg += struct.pack(">4shhhhBB", inet_aton(self._my_ip), self._controlPort, 0x7400, 0x1f00, 0x0221, 0, 0) # last 0 is a checksum
-        printHex(msg)
-        print len(msg)
-        self._s.send(msg)
-        resp = self._s.recv(128)
-        print "recv:"
-        printHex(resp)
+            tmp = data[18:]
+            resp, name, checksum = struct.unpack(">H32sB", tmp)
+            return name
+        return None
+
+def setupArgs():
+    parser = argparse.ArgumentParser(description="LK373A compatible control utility")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--ip', help="IP of device to manage")
+    group.add_argument('-l', '--listen', help="Listen for units broadcasting their existence", action="store_true")
+#    group.add_argument('-s', '--search', help="Search for units via UDP broadcast", action="store_true")
+
+    return parser
 
 if __name__ == '__main__':
     # register SIGINT handler to exit
     signal.signal(signal.SIGINT, signal_handler)
 
-    q = Queue()
-    listen_thread = Thread(target=broadcastListener, args=(q, lambda: stop_threads))
-    listen_thread.start()
+    parser = setupArgs()
+    args = parser.parse_args()
+    #print args
 
-    d = IPTV_DeviceControl('10.10.10.187')
-    print d.connect()
-    d.GetName()
+    # if we're not broadcasting and haven't been given an IP, listen
+    if not args.ip:# and not args.search:
+        args.listen = True
+
+    q = Queue()
+
+    if args.listen:
+        listen_thread = Thread(target=broadcastListener, args=(q, lambda: stop_threads))
+        listen_thread.start()
+
+    if args.ip:
+        q.put(args.ip)
+
     while True:
-        signal.pause()
+        try:
+            ip = q.get(timeout=0.1)
+            d = IPTV_DeviceControl(ip)
+            name = d.GetName()
+            if not name:
+                print "Failed to get device name"
+            else:
+                print "Device name is %s" % name
+        except Empty:
+            if args.ip:
+                sys.exit(0)
+            pass
+  #  while True:
+   #     signal.pause()
 
